3c599a7394e6ffd4acf67115d69480dd5c45a902
madhavagrawal356-netizen <madhav.agrawal356@gmail.com>
2026-07-02 00:46:37 +0530

    Integrate ARIMA model evaluation into evaluate function
    Removed LightGBM model evaluation and added ARIMA model evaluation to the evaluate function.

--- main_files/evaluate.py
+++ main_files/evaluate.py
@@ -4,11 +4,12 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
-from .trainer import train_xgb, train_lgb , prepare_data, train_rf
+from .trainer import train_xgb, prepare_data, train_rf
 from .data_loader import load_stock
 
 # %%
 from .features import create_features
+from statsmodels.tsa.arima.model import ARIMA
 
 
 # %%
@@ -27,11 +28,23 @@ def quick_evaluate(ticker):
     df = create_features(df)
     X_train, X_test, y_train, y_test = prepare_data(df)
     rmse_xgb, mae_xgb, r2_xgb = evaluate(train_xgb(X_train, y_train), X_test, y_test)
-    rmse_lgb, mae_lgb, r2_lgb = evaluate(train_lgb(X_train, y_train), X_test, y_test)
+    
     rmse_rf, mae_rf, r2_rf = evaluate(train_rf(X_train, y_train), X_test, y_test)
-    lowest_rmse = min([rmse_xgb, rmse_lgb, rmse_rf])
-    best_model = train_xgb(X_train, y_train) if lowest_rmse == rmse_xgb else train_lgb(X_train, y_train) if lowest_rmse == rmse_lgb else train_rf(X_train, y_train)
-    model_name = best_model.__class__.__name__
+    arima_model = ARIMA(y_train, order=(5,1,0)).fit()
+    arima_pred = arima_model.forecast(steps=len(y_test))
+    rmse_arima = np.sqrt(mean_squared_error(y_test, arima_pred))
+    mae_arima = mean_absolute_error(y_test, arima_pred)
+    r2_arima = r2_score(y_test, arima_pred)
+    lowest_rmse = min([rmse_xgb, rmse_arima, rmse_rf])
+    if lowest_rmse == rmse_xgb:
+        best_model = train_xgb(X_train, y_train)
+        model_name = 'XGBoost'
+    elif lowest_rmse==rmse_rf:
+        best_model = train_rf(X_train, y_train)
+        model_name = 'RandomForest'
+    else:
+        best_model=arima_model
+        model_name='ARIMA'
     return best_model , lowest_rmse, model_name, X_test, y_test, df2
 
 # %%
